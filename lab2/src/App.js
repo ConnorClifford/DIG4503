5a1df8e8c1105401b1d8ad4400d7f5808fc23167
Connor <connorclifford17@gmail.com>
2020-01-25 02:55:58 -0500

    Add files via upload

---
+++ lab2/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import HomePage from './components/HomePage';
+
+class App extends React.Component {
+  render() {
+    return (
+          <HomePage firstName = 'Connor'></HomePage>
+    );
+  }
+}
+
+export default App;
