0ce3dfc449cd518a83b97600c16717319108f941
Connor <connorclifford17@gmail.com>
2020-02-15 13:06:05 -0500

    lab5 done.

--- lab5/index.js
+++ lab5/index.js
@@ -1,54 +1,53 @@
-const Express = require("express");
+//------declare imported vars--------
+const Express = require ("express");
 const App = Express();
 const port = 80;
+const chalk = require("Chalk"); 
 
-let database = [
-    {
-        "name" : "Birds of Prey",
-        "score": 79
-    },
-    {
-        "name": "Cats",
-        "score": 20
-    },
-    {
-        "name": "Interstellar",
-        "score": 72
-    },
-    {
-        "name": "Parasite",
-        "score": 99
-    }
-    
-];
+const pokemons = require("json-pokemon");
+const pokeArray = pokemons;
 
+//-------------------------------------
 App.use(Express.json());
 App.use("/", Express.static("public"));
 
-App.get("/api/movies/name/:name", (req, res) =>{
-    let result = {"error": "Error Message!"};
-
-    database.forEach((value) => {
-      if(req.params.name == value.name){
-            result = value;
+App.get("/name/:name", (req,res)=>{
+     
+    let result = {"error": "Nothing found that matches that. :C"};
+    
+    pokeArray.forEach((value)=>{
+        if(value.name.toLowerCase() == req.params.name){
+             let index = value.id - 1;
+             result = pokeArray[index];
+             console.log(chalk.green(result.name));
         }
     });
 
+    if(result.error){
+        console.log(chalk.red(result.name));
+    }
     res.json(result);
 });
 
-App.get("/api/movies/score/:score", (req, res) =>{
-    let result = {"error": "Error Message!"};
-
-    database.forEach((value) => {
-      if(req.params.score == value.score){
-            result = value;
+App.get("/id/:id", (req, res)=>{
+    
+    let result = {"error": "Nothing found that matches that. :C"};
+    
+    pokeArray.forEach((value)=>{
+        if(value.id == req.params.id){
+             let index = value.id - 1;
+             result = pokeArray[index];
+             console.log(chalk.green(result.id));
         }
     });
-
+//debugging
+    //console.log(result);
+    if(result.error){
+        console.log(chalk.red(result.id));
+    }
     res.json(result);
-});
+}); 
 
-App.listen(port, () =>{
-    console.log("server running!");
-}) 
+App.listen(port,()=>{
+    console.log("Server is up!");
+});
